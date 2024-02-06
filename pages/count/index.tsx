03cb773c1111fa0e32a7b21070656b437159a16e
PhanNhatLoi <phanloi971@gmail.com>
2024-02-06 10:24:58 +0700

    change domain

--- pages/count/index.tsx
+++ pages/count/index.tsx
@@ -4,20 +4,4 @@ function CountPage() {
   return <div>CountPage</div>;
 }
 
-// pages/server-sitemap-index.xml/index.tsx
-import { getServerSideSitemapIndexLegacy } from 'next-sitemap';
-import { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async ctx => {
-  // Method to source urls from cms
-  // const urls = await fetch('https//example.com/api')
-
-  return getServerSideSitemapIndexLegacy(ctx, [
-    'http://localhost:3003/path-1.xml',
-    'http://localhost:3003/path-2.xml'
-  ]);
-};
-
-// Default export to prevent next.js errors
-
 export default CountPage;
