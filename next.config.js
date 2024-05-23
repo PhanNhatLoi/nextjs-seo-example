5b48bb45f4bfb6c83f06d754c756d1c2e64470d7
PhanNhatLoi <phanloi971@gmail.com>
2024-05-23 17:22:46 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: {
+    plugins: [
+      new Dotenv({
+        path: path.join(__dirname, '.env')
+      })
+    ]
+  }
+};
 
 module.exports = nextConfig;
